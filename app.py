396d4a8b13f2b4027e1547dcd2cb636cce8f9e7f
Leonel <LeonelGodon@gmail.com>
2025-10-14 01:53:49 -0400

    as of 10-14-2025

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def streamer():
     # A list of up to 100 streamer logins
     streamers = ['cm_nyc', 'snoozefighting', 'scentless_apprentice', 'Rinzson', 'extrahotchicken', 'clearjoker',
                  'chato__', 'keokeofofeo', 'crispyjenny', 'mommygivememilk', 'philski', 'sattamxSAM',
-                 'FaruIRL', 'Domorobogato', 'babubird', 'stinkycarnival', 'commandercooder', 'hazelbunni', 'luxcess']
+                 'FaruIRL', 'Domorobogato', 'babubird', 'stinkycarnival', 'commandercooder', 'hazelbunni', 'luxcess', 'ulumaika']
     dicts = {}
 
     # Create the URL query string by joining streamer names
